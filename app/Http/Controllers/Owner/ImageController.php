8499dd423d08b68c45e63547b5f5b314bc31a8c1
Kenya Ishizuka <ken8116@icloud.com>
2022-08-07 21:55:08 +0900

    edit, update

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -56,8 +56,8 @@ class ImageController extends Controller
     public function store(UploadImageRequest $request)
     {
         $imageFiles = $request->file('files');
-        if(!is_null($imageFiles)) {
-            foreach($imageFiles as $imageFile) {
+        if (!is_null($imageFiles)) {
+            foreach ($imageFiles as $imageFile) {
                 $fileNameToStore = ImageService::upload($imageFile, 'products');
                 Image::create([
                     'owner_id' => Auth::id(),
@@ -69,16 +69,6 @@ class ImageController extends Controller
         return redirect()->route('owner.images.index')->with(['message' => '画像登録を実施しました', 'status' => 'info']);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
 
     /**
      * Show the form for editing the specified resource.
@@ -88,7 +78,9 @@ class ImageController extends Controller
      */
     public function edit($id)
     {
-        //
+        $image = Image::findOrFail($id);
+
+        return view('owner.images.edit', compact('image'));
     }
 
     /**
@@ -100,7 +92,15 @@ class ImageController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'title' => 'string|max:50',
+        ]);
+
+        $image = Image::findOrFail($id);
+        $image->title = $request->title;
+        $image->save();
+
+        return redirect()->route('owner.images.index')->with(['message' => '画像情報を更新しました。', 'status' => 'info']);
     }
 
     /**
