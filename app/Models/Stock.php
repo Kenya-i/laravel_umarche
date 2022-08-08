5b782d03a6696bc93e58492771cc610cef9c9baf
Kenya Ishizuka <ken8116@icloud.com>
2022-08-09 00:04:02 +0900

    prodcut store

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -9,6 +9,11 @@ class Stock extends Model
 {
     use HasFactory;
 
-
     protected $table = 't_stocks';
+
+    protected $fillable = [
+        'product_id',
+        'type',
+        'quantity'
+    ];
 }
