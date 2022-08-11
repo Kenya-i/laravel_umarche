53ba971312dc83140a76195a89d95e21bcaea40f
Kenya Ishizuka <ken8116@icloud.com>
2022-08-11 23:48:20 +0900

    User queryその1

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -5,13 +5,28 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
 {
 
     public function index()
     {
-        $products = Product::all();
+        $stocks = DB::table('t_stocks')
+            ->select('product_id', DB::raw('sum(quantity) as quantity'))
+            ->groupBy('product_id')
+            ->having('quantity', '>', 1);
+
+
+        $products = DB::table('products')->joinSub($stocks, 'stock', function ($join) {
+            $join->on('products.id', '=', 'stock.product_id');
+        })
+            ->join('shops', 'products.shop_id', '=', 'shops.id')
+            ->where('shops.is_selling', true)
+            ->where('products.is_selling', true)
+            ->get();
+
+
         return view('user.index', compact('products'));
     }
 }
